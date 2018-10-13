809b8dc180ed0b6dfa99cbd67956f161cb3c3bdd
Sinhika <sinhika@republicofnewhome.org>
2018-10-13 09:01:40 -0500

    added skeletons of recipe conditions that will be required

---
+++ src/main/java/alexndr/plugins/Aesthetics/conditions/NetherrocksConditionFactory.java
@@ -0,0 +1,18 @@
+package alexndr.plugins.Aesthetics.conditions;
+
+import java.util.function.BooleanSupplier;
+
+import com.google.gson.JsonObject;
+
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class NetherrocksConditionFactory implements IConditionFactory {
+
+	@Override
+	public BooleanSupplier parse(JsonContext context, JsonObject json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/alexndr/plugins/Aesthetics/conditions/VanillaConditionFactory.java
@@ -0,0 +1,20 @@
+package alexndr.plugins.Aesthetics.conditions;
+
+import java.util.function.BooleanSupplier;
+
+import com.google.gson.JsonObject;
+
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class VanillaConditionFactory implements IConditionFactory 
+{
+
+	@Override
+	public BooleanSupplier parse(JsonContext context, JsonObject json) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+} // end class
