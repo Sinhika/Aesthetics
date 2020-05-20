8245cdb6de5e9a950abb1d99f77bd77eef10c256
Sinhika <sinhika@republicofnewhome.org>
2020-05-20 13:44:54 -0500

    start of config

---
+++ src/main/java/mod/alexndr/aesthetics/config/AestheticsConfig.java
@@ -0,0 +1,32 @@
+package mod.alexndr.aesthetics.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import mod.alexndr.simpleores.api.config.ISimpleConfig;
+
+public class AestheticsConfig implements ISimpleConfig
+{
+    private static Map<String, Boolean> flags = new HashMap<>();
+    public static AestheticsConfig INSTANCE = new AestheticsConfig();
+    
+    @Override
+    public void clear()
+    {
+        flags.clear();
+    }
+
+    @Override
+    public boolean getFlag(String n)
+    {
+        Boolean obj = flags.get(n);
+        return obj != null && obj;
+    }
+
+    @Override
+    public void putFlag(String n, boolean val)
+    {
+        flags.put(n, val);
+    }
+
+} // end class
