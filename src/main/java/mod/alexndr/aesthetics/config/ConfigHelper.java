2007c9b87b4f4f51591778b0d8c603ef7d7c12d3
Sinhika <sinhika@republicofnewhome.org>
2020-05-21 11:06:46 -0500

    disable recipes for mods that aren't loaded

--- src/main/java/mod/alexndr/aesthetics/config/ConfigHelper.java
+++ src/main/java/mod/alexndr/aesthetics/config/ConfigHelper.java
@@ -1,5 +1,6 @@
 package mod.alexndr.aesthetics.config;
 
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.config.ModConfig;
 
 public final class ConfigHelper
@@ -12,10 +13,16 @@ public final class ConfigHelper
                                           ConfigHolder.SERVER.serverEnableVanillaBlocks.get());
         AestheticsConfig.INSTANCE.putFlag("simpleores_enabled", 
                                           ConfigHolder.SERVER.serverEnableSimpleOresBlocks.get());
-        AestheticsConfig.INSTANCE.putFlag("netherrocks_enabled", 
-                                           ConfigHolder.SERVER.serverEnableNetherrocksBlocks.get());
-        AestheticsConfig.INSTANCE.putFlag("fusion_enabled", 
-                                           ConfigHolder.SERVER.serverEnableFusionBlocks.get());
+        
+        /* disable Netherrocks recipes if Netherrocks is not loaded */
+        boolean val = ConfigHolder.SERVER.serverEnableNetherrocksBlocks.get() 
+                && ModList.get().isLoaded("netherrocks");
+        AestheticsConfig.INSTANCE.putFlag("netherrocks_enabled", val);
+        
+        /* disable Fusion recipes if Fusion is not loaded */
+        val = ConfigHolder.SERVER.serverEnableFusionBlocks.get() 
+                && ModList.get().isLoaded("fusion");
+        AestheticsConfig.INSTANCE.putFlag("fusion_enabled", val);
     } // end bakeServer()
 
 } // end class
