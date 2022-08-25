e23d240b4f9e789bed593d01c72691c6ad93b1bc
Sinhika <sinhika@republicofnewhome.org>
2022-08-25 14:44:11 -0500

    removed Netherrocks and Fusion Aesthetics blocks

--- src/main/java/mod/alexndr/aesthetics/init/ModBlocks.java
+++ src/main/java/mod/alexndr/aesthetics/init/ModBlocks.java
@@ -1,7 +1,6 @@
 package mod.alexndr.aesthetics.init;
 
 import mod.alexndr.aesthetics.Aesthetics;
-import mod.alexndr.simplecorelib.helpers.LightUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoorBlock;
@@ -38,27 +37,6 @@ public final class ModBlocks
     public static RegistryObject<Block> diamond_bricks = BLOCKS.register("diamond_bricks",
             () -> new Block(Block.Properties.copy(Blocks.DIAMOND_BLOCK)));
     
-    // Blocks - bricks - Netherrocks
-    public static RegistryObject<Block> argonite_bricks = BLOCKS.register("argonite_bricks",
-            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.COLOR_BLUE)
-                    .strength(7.0F, 72.0F).sound(SoundType.METAL).requiresCorrectToolForDrops()));
-    public static RegistryObject<Block> ashstone_bricks = BLOCKS.register("ashstone_bricks",
-            () -> new Block(Block.Properties.of(Material.STONE)
-                    .strength(7.0F, 72.0F).requiresCorrectToolForDrops()));
-    public static RegistryObject<Block> dragonstone_bricks = BLOCKS.register("dragonstone_bricks",
-            () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.NETHER)
-                    .strength(10.0F, 72.0F).requiresCorrectToolForDrops()));
-    public static RegistryObject<Block> fyrite_bricks = BLOCKS.register("fyrite_bricks",
-            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.FIRE)
-                    .strength(7.0F, 72.0F).sound(SoundType.METAL).requiresCorrectToolForDrops()));
-    public static RegistryObject<Block> illumenite_bricks = BLOCKS.register("illumenite_bricks",
-            () -> new Block(Block.Properties.of(Material.GLASS, MaterialColor.COLOR_YELLOW)
-                    .strength(7.0F, 72.0F).sound(SoundType.GLASS).requiresCorrectToolForDrops()
-                    .lightLevel(LightUtils.setFixedLight(15))));
-    public static RegistryObject<Block> malachite_bricks = BLOCKS.register("malachite_bricks",
-            () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.EMERALD)
-                    .strength(7.0F, 72.0F).sound(SoundType.METAL).requiresCorrectToolForDrops()));
-
     // Blocks - bricks - Fusion
     public static RegistryObject<Block> bronze_bricks = BLOCKS.register("bronze_bricks",
             () -> new Block(Block.Properties.of(Material.METAL, MaterialColor.TERRACOTTA_YELLOW)
@@ -84,26 +62,6 @@ public final class ModBlocks
             () -> new StairBlock( () -> diamond_bricks.get().defaultBlockState(), 
                                    Block.Properties.copy(diamond_bricks.get())));
 
-    // Blocks - stairs - netherrocks
-    public static RegistryObject<StairBlock> argonite_brick_stairs = BLOCKS.register("argonite_brick_stairs", 
-            () -> new StairBlock( () -> argonite_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(argonite_bricks.get())));
-    public static RegistryObject<StairBlock> ashstone_brick_stairs = BLOCKS.register("ashstone_brick_stairs", 
-            () -> new StairBlock( () -> ashstone_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(ashstone_bricks.get())));
-    public static RegistryObject<StairBlock> dragonstone_brick_stairs = BLOCKS.register("dragonstone_brick_stairs", 
-            () -> new StairBlock( () -> dragonstone_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(dragonstone_bricks.get())));
-    public static RegistryObject<StairBlock> fyrite_brick_stairs = BLOCKS.register("fyrite_brick_stairs", 
-            () -> new StairBlock( () -> fyrite_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(fyrite_bricks.get())));
-    public static RegistryObject<StairBlock> illumenite_brick_stairs = BLOCKS.register("illumenite_brick_stairs", 
-            () -> new StairBlock( () -> illumenite_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(illumenite_bricks.get())));
-    public static RegistryObject<StairBlock> malachite_brick_stairs = BLOCKS.register("malachite_brick_stairs", 
-            () -> new StairBlock( () -> malachite_bricks.get().defaultBlockState(), 
-                                   Block.Properties.copy(malachite_bricks.get())));
-   
     // Blocks - stairs - fusion
     public static RegistryObject<StairBlock> bronze_brick_stairs = BLOCKS.register("bronze_brick_stairs", 
             () -> new StairBlock( () -> bronze_bricks.get().defaultBlockState(), 
@@ -118,45 +76,10 @@ public final class ModBlocks
             () -> new StairBlock( () -> thyrium_bricks.get().defaultBlockState(), 
                                    Block.Properties.copy(thyrium_bricks.get())));
   
-    // Blocks - doors - netherrocks
-    public static RegistryObject<DoorBlock> argonite_door = BLOCKS.register("argonite_door",
-            () -> new DoorBlock(Block.Properties.copy(argonite_bricks.get()).noOcclusion()));
-    public static RegistryObject<DoorBlock> ashstone_door = BLOCKS.register("ashstone_door",
-            () -> new DoorBlock(Block.Properties.copy(ashstone_bricks.get()).noOcclusion()));
-    public static RegistryObject<DoorBlock> dragonstone_door = BLOCKS.register("dragonstone_door",
-            () -> new DoorBlock(Block.Properties.copy(dragonstone_bricks.get()).noOcclusion()));
-    public static RegistryObject<DoorBlock> fyrite_door = BLOCKS.register("fyrite_door",
-            () -> new DoorBlock(Block.Properties.copy(fyrite_bricks.get()).noOcclusion()));
-    public static RegistryObject<DoorBlock> illumenite_door = BLOCKS.register("illumenite_door",
-            () -> new DoorBlock(Block.Properties.copy(illumenite_bricks.get()).noOcclusion()));
-    public static RegistryObject<DoorBlock> malachite_door = BLOCKS.register("malachite_door",
-            () -> new DoorBlock(Block.Properties.copy(malachite_bricks.get()).noOcclusion()));
-   
     // Blocks - doors - fusion
     public static RegistryObject<DoorBlock> bronze_door = BLOCKS.register("bronze_door",
             () -> new DoorBlock(Block.Properties.copy(bronze_bricks.get()).noOcclusion()));
     
-    // Blocks - bars - netherrocks
-    public static RegistryObject<IronBarsBlock> argonite_bars = BLOCKS.register("argonite_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.METAL, MaterialColor.NONE)
-                    .strength(7.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).noOcclusion()));
-    public static RegistryObject<IronBarsBlock> ashstone_bars = BLOCKS.register("ashstone_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.STONE, MaterialColor.NONE)
-                    .strength(7.0F).requiresCorrectToolForDrops().sound(SoundType.STONE).noOcclusion()));
-    public static RegistryObject<IronBarsBlock> dragonstone_bars = BLOCKS.register("dragonstone_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.STONE, MaterialColor.NONE)
-                    .strength(10.0F).requiresCorrectToolForDrops().sound(SoundType.STONE).noOcclusion()));
-    public static RegistryObject<IronBarsBlock> fyrite_bars = BLOCKS.register("fyrite_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.METAL, MaterialColor.NONE)
-                    .strength(7.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).noOcclusion()));
-    public static RegistryObject<IronBarsBlock> illumenite_bars = BLOCKS.register("illumenite_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.METAL, MaterialColor.NONE)
-                    .strength(7.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).noOcclusion()
-                    .lightLevel(LightUtils.setFixedLight(14))));
-    public static RegistryObject<IronBarsBlock> malachite_bars = BLOCKS.register("malachite_bars",
-            () -> new IronBarsBlock(Block.Properties.of(Material.METAL, MaterialColor.NONE)
-                    .strength(7.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).noOcclusion()));
-   
     // Blocks - bars - fusion
     public static RegistryObject<IronBarsBlock> bronze_bars = BLOCKS.register("bronze_bars",
             () -> new IronBarsBlock(Block.Properties.of(Material.METAL, MaterialColor.NONE)
